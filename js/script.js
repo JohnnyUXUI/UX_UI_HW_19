5ed09b1df1b9d5a09945014d3480de855049e2fd
JohnnyUXUI <johnny.jakubowicz@mail.utoronto.ca>
2021-11-20 12:39:26 -0500

    add more interaction

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 const loadText = document.querySelector('.loading-text')
 const bg = document.querySelector('.bg')
+const bgColour = document.querySelector('.bg-colour')
 
 let load = 0
 
-let int = setInterval(blurring, 5)
+let int = setInterval(blurring, 10)
 
 function blurring() {
   load++
@@ -14,7 +15,7 @@ function blurring() {
 
   loadText.innerText = `${load}%`
   loadText.style.opacity = scale(load, 0, 100, 1, 0)
-  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+  bgColour.style.opacity = scale(load, 0, 100, 1, 0)
 }
 
 const scale = (num, in_min, in_max, out_min, out_max) => {
